8e5d3ebee677036bfe30d117b3232c358c6682c6
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-12-08 14:37:04 +0500

    added .h files for linkedlist and hashtable

---
+++ hashTable.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "list.h" // Include your custom List class
+
+using namespace std;
+
+class HashTable {
+private:
+    static const int SIZE = 10;
+    list table[SIZE]; // Array of custom lists for handling collisions
+
+    int hashFunction(int key) const {
+        return key % SIZE; // Simple modulo-based hash function
+    }
+
+public:
+    void insert(int key, const string& value) {
+        int index = hashFunction(key);
+        Node* existing = table[index].find(to_string(key));
+        if (existing != nullptr) {
+            existing->data = to_string(key) + ":" + value; // Update if key exists
+        } else {
+            table[index].push_back(to_string(key) + ":" + value);
+        }
+    }
+
+    string find(int key) const {
+        int index = hashFunction(key);
+        Node* temp = table[index].getHead();
+        while (temp != nullptr) {
+            size_t pos = temp->data.find(':');
+            if (pos != string::npos) {
+                int storedKey = stoi(temp->data.substr(0, pos));
+                if (storedKey == key) {
+                    return temp->data.substr(pos + 1); // Return value
+                }
+            }
+            temp = temp->next;
+        }
+        return "---"; // Default value if not found
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,220 +1,50 @@
 #include <iostream>
+#include <ctime>
+#include <sstream>
+#include <stdexcept>
+#include <iomanip>
+#include "list.h"
+#include "hashTable.h"
 using namespace std;
 
-class Node
+string curr_time();
+class TreeNode
 {
 public:
-    string data; // The data stored in the node
-    Node *prev;  // Pointer to the previous node
-    Node *next;  // Pointer to the next node
+    string name;
+    list contents;
+    char type;
+    string cdate;
+    string mdate;
+    int permission;
+    TreeNode* parent;
+    TreeNode* link;
+    TreeNode* child;
 
-    Node(const string &value){
-        data=value;
-        prev=nullptr;
-        next=nullptr;
-    }
-};
+    TreeNode(TreeNode* pwd, const string& name)
+        : name(name), parent(pwd), permission(6),
+        cdate(curr_time()), mdate(curr_time()),
+        link(nullptr), child(nullptr) {}
 
-class List{
-private:
-    Node *head; // Pointer to the first node
-    Node *tail; // Pointer to the last node
+   string get_permission() const {
+    static HashTable permissions = []() {
+        HashTable ht;
+        ht.insert(0, "---");
+        ht.insert(1, "--x");
+        ht.insert(2, "-w-");
+        ht.insert(3, "-wx");
+        ht.insert(4, "r--");
+        ht.insert(5, "r-x");
+        ht.insert(6, "rw-");
+        ht.insert(7, "rwx");
+        return ht;
+    }();
 
-public:
- List(){
-    head=nullptr;
-    tail=nullptr;
- }
+    return permissions.find(permission);
+}
 
-    void push_front(const string &value)
-    { // Push a value to the front
-        Node *newNode = new Node(value);
-        if (head == nullptr)
-        {
-            head = tail = newNode;
-        }
-        else
-        {
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
-        }
-    }
 
-    void push_back(const string &value)
-    { // Push a value to the back
-        Node *newNode = new Node(value);
-        if (tail == nullptr)
-        {
-            head = tail = newNode;
-        }
-        else
-        {
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
-        }
-    }
-
-    void pop_front()
-    { // Pop a value from the front
-        if (head == nullptr)
-        {
-            return;
-        }
-        Node *temp = head;
-        head = head->next;
-        if (head != nullptr)
-            head->prev = nullptr;
-        delete temp;
-        if (head == nullptr)
-        {
-            tail = nullptr;
-        }
-    }
-
-    // Pop a value from the back
-    void pop_back()
-    {
-        if (tail == nullptr)
-        {
-            return;
-        }
-        Node *temp = tail;
-        tail = tail->prev;
-        if (tail != nullptr)
-            tail->next = nullptr;
-        delete temp;
-        if (tail == nullptr)
-        {
-            head = nullptr;
-        } 
-    }
-
-    // Insert a value at a specific position
-    void insert(Node *pos, const string &value)
-    {
-        if (pos == nullptr)
-            return;
-        Node *newNode = new Node(value);
-        newNode->next = pos;
-        newNode->prev = pos->prev;
-        if (pos->prev != nullptr)
-        {
-            pos->prev->next = newNode;
-        }
-        else
-        {
-            head = newNode;
-        }
-        pos->prev = newNode;
-    }
-
-    // Erase a node at a specific position
-    void erase(Node *pos)
-    {
-        if (pos == nullptr)
-            return;
-        if (pos->prev != nullptr)
-        {
-            pos->prev->next = pos->next;
-        }
-        else
-        {
-            head = pos->next;
-        }
-        if (pos->next != nullptr)
-        {
-            pos->next->prev = pos->prev;
-        }
-        else
-        {
-            tail = pos->prev;
-        }
-        delete pos;
-    }
-
-    // Find a node by its value
-    Node *find(const string &value)
-    {
-        Node *temp = head;
-        while (temp != nullptr)
-        {
-            if (temp->data == value)
-            {
-                return temp;
-            }
-            temp = temp->next;
-        }
-        return nullptr;
-    }
-
-    // Check if the list is empty
-    bool empty() const
-    {
-        return head == nullptr;
-    }
-
-    // Get the size of the list (number of elements)
-    size_t size() const
-    {
-        size_t count = 0;
-        Node *temp = head;
-        while (temp != nullptr)
-        {
-            count++;
-            temp = temp->next;
-        }
-        return count;
-    }
-
-    // Display the list from front to back
-    void displayForward() const
-    {
-        Node *temp = head;
-        while (temp != nullptr)
-        {
-            cout << temp->data << " ";
-            temp = temp->next;
-        }
-        cout << endl;
-    }
-
-    // Display the list from back to front
-    void displayBackward() const
-    {
-        Node *temp = tail;
-        while (temp != nullptr)
-        {
-            cout << temp->data << " ";
-            temp = temp->prev;
-        }
-        cout << endl;
-    }
-
-    // Destructor to free up memory
-    ~List()
-    {
-        Node *temp = head;
-        while (temp != nullptr)
-        {
-            Node *nextNode = temp->next;
-            delete temp;
-            temp = nextNode;
-        }
-    }
-
-    // Get the head of the list
-    Node *getHead() const
-    {
-        return head;
-    }
-
-    // Get the tail of the list
-    Node *getTail() const
-    {
-        return tail;
-    }
+    ~TreeNode() = default;
 };
 
 void print_help()
@@ -239,6 +69,19 @@ void print_help()
     cout << "\tchmod M P -   change permissions of the file at path P to mode M" << endl;
     cout << "\tclear     -   clear the console screen" << endl;
     cout << "\texit      -   exit the shell" << endl;
+}
+
+string curr_time() {
+    time_t t = time(nullptr);
+    struct tm tm;
+
+    if (localtime_s(&tm, &t) != 0) {
+        throw std::runtime_error("Failed to get local time");
+    }
+
+    ostringstream oss;
+    oss << put_time(&tm, "%Y-%m-%d %H:%M:%S");
+    return oss.str();
 }
 
 int main()
