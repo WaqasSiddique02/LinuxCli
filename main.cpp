a710c4b36eecf1af9c661a8464ae944d4183a72c
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-12-22 15:16:38 +0500

    added history feature

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include "list.h"
 #include "hashTable.h"
 #include "tree.h"
+#include "stack.h"
 
 using namespace std;
 
 void print_help()
 {
     cout << "*** Follows the syntax of Linux shell commands ***" << endl
-        << endl;
+         << endl;
     cout << "\thelp      -   print this message" << endl;
     cout << "\tls        -   list contents of the current directory" << endl;
     cout << "\ttree      -   list contents of the current directory in a tree-like format" << endl;
@@ -25,75 +26,116 @@ void print_help()
     cout << "\tedit P    -   edit the file at path P" << endl;
     cout << "\tcat P     -   print the contents of the file at path P" << endl;
     cout << "\tchmod M P -   change permissions of the file at path P to mode M" << endl;
+    cout << "\thistory   -   show command history" << endl;
     cout << "\tclear     -   clear the console screen" << endl;
     cout << "\texit      -   exit the shell" << endl;
 }
 
-int main() {    
-    TreeNode* root = new TreeNode(nullptr, "");
-    TreeNode* pwd = root;                    
-    
+int main()
+{
+    TreeNode *root = new TreeNode(nullptr, "");
+    TreeNode *pwd = root;
+
     print_help();
-    linux_tree(root);      
+    linux_tree(root);
+
+    stack commandHistory; // Initialize a stack to store the history
+
     string command;
-    while (true) {
+    while (true)
+    {
         cout << ">> ";
         cin >> command;
 
-        if (command == "help") {
+        // Push the command to the history stack
+        commandHistory.push(command);
+
+        if (command == "help")
+        {
             print_help();
         }
-        else if (command == "ls") {
+        else if (command == "ls")
+        {
             print_ls(pwd);
         }
-        else if (command == "stat") {
+        else if (command == "stat")
+        {
             string path;
             cin >> path;
+            command += " " + path;        // Append the argument to the command for history
+            commandHistory.push(command); // Update command history
             print_stat(root, pwd, path);
         }
-        else if (command == "pwd") {
+        else if (command == "pwd")
+        {
             cout << pwd_str(pwd) << endl;
         }
-        else if (command == "cd") {
+        else if (command == "cd")
+        {
             string path;
             cin >> path;
-            TreeNode* newPwd = cd(root, pwd, path);
-            if (newPwd != nullptr) {
+            command += " " + path;
+            TreeNode *newPwd = cd(root, pwd, path);
+            if (newPwd != nullptr)
+            {
                 pwd = newPwd;
             }
         }
-        else if (command == "mkdir") {
+        else if (command == "mkdir")
+        {
             string path;
             cin >> path;
+            command += " " + path;
             create(root, pwd, path, 'd');
         }
-        else if (command == "touch") {
+        else if (command == "touch")
+        {
             string path;
             cin >> path;
+            command += " " + path;
             create(root, pwd, path, '-');
         }
-        else if (command == "rm") {
+        else if (command == "rm")
+        {
             string path;
             cin >> path;
+            command += " " + path;
             remove(root, pwd, path);
         }
-        else if (command == "chmod") {
+        else if (command == "chmod")
+        {
             string path, mode;
             cin >> path >> mode;
+            command += " " + path + " " + mode;
             chmod(root, pwd, path, mode);
         }
-        else if (command == "clear") {
+        else if (command == "history")
+        {
+            cout << "Command History:" << endl;
+            stack tempStack = commandHistory; // Make a copy of the command history
+            int index = 1;
+
+            while (!tempStack.isEmpty())
+            {
+                cout << index++ << ": " << tempStack.getTop() << endl; // Use getTop() instead of top()
+                tempStack.pop();
+            }
+        }
+        else if (command == "clear")
+        {
             clear_screen();
         }
-        else if (command == "exit") {
+        else if (command == "exit")
+        {
             cout << "Exiting file system simulator. Goodbye!" << endl;
             break;
         }
-        else {
+        else
+        {
             cout << "Unknown command. Type 'help' for the list of commands." << endl;
         }
     }
 
-    delete root; 
+    delete root;
     return 0;
 }
--- stack.h
+++ stack.h
@@ -1,55 +1,99 @@
+#pragma once
 #include <iostream>
 #include <string>
 using namespace std;
 
-struct StackNode {
-    string command;
-    StackNode* next;
-    
-    StackNode(const string& cmd) : command(cmd), next(nullptr) {}
-};
-
-class CommandStack {
+class stack {
 private:
+    class StackNode {
+    public:
+        string command;
+        StackNode* next;
+
+        // Constructor for StackNode
+        StackNode(const string& cmd) {
+            command = cmd;
+            next = nullptr;
+        }
+    };
+
     StackNode* top;
 
 public:
-    CommandStack() : top(nullptr) {}
-
-    void push(const string& command) {
-        StackNode* newNode = new StackNode(command);
-        newNode->next = top;
-        top = newNode; 
+    // Constructor for stack
+    stack() {
+        top = nullptr;  // Stack starts as empty
     }
 
+    // Method to push a command onto the stack
+    void push(const string& command) {
+        // Create a new node with the given command
+        StackNode* newNode = new StackNode(command);
+
+        // If the stack is empty, new node becomes the top
+        if (top == nullptr) {
+            top = newNode;
+        } else {
+            // Otherwise, insert the new node at the top of the stack
+            newNode->next = top;
+            top = newNode;
+        }
+    }
+
+    // Method to pop the top command from the stack
     string pop() {
-        if (!top) {
+        if (top == nullptr) {
+            // Stack is empty, no commands to pop
             cout << "No command in history!" << endl;
             return "";
+        } else {
+            // Store the command to return
+            StackNode* temp = top;
+            string command = top->command;
+
+            // Move the top pointer to the next node
+            top = top->next;
+
+            // Delete the old top node
+            delete temp;
+            return command;
         }
-        StackNode* temp = top;
-        string command = top->command;
-        top = top->next;
-        delete temp;
-        return command;
     }
 
+    // Method to peek at the top command without removing it
     string peek() const {
-        if (!top) {
+        if (top == nullptr) {
+            // Stack is empty
             return "";
+        } else {
+            return top->command;
         }
-        return top->command;
     }
 
-    bool is_empty() const {
+    // Method to check if the stack is empty
+    bool isEmpty() const {
         return top == nullptr;
     }
 
-    ~CommandStack() {
-        while (top) {
-            StackNode* temp = top;
-            top = top->next;
+    string getTop() const {
+        if (top == nullptr) {
+            // Stack is empty
+            return "";
+        } else {
+            return top->command;
+        }
+    }
+
+    // Destructor to clean up the stack memory
+    ~stack() {
+        StackNode* current = top;
+
+        // Traverse and delete each node
+        while (current != nullptr) {
+            StackNode* temp = current;
+            current = current->next;
             delete temp;
         }
+        top = nullptr;  // Explicitly setting top to nullptr after deletion
     }
 };
